2aa536e8404564aa030eb0832f79956e35340256
Tygh Walters <tyghwalters@gmail.com>
2022-09-27 22:11:11 -0700

    Add initial methods, classes, and game start sequence

--- hangman.rb
+++ hangman.rb
@@ -1,14 +1,39 @@
 class Hangman
-# load Google dictionary
-# randomly select a word between 5 and 12 characters long for the secret word.
+  # load Google dictionary
+  # randomly select a word between 5 and 12 characters long for the secret word.
+  attr_accessor :player 
 
-def display_board
-puts "_ _ _ _ _ "
-end
+  def initialize(player)
+  @player = player 
+  end
 
-end
+  def display_board
+    puts "Board: _ _ _ _ _ "
+    winner_check
+  end
+
+  def winner_check
+    puts "Winner!"
+  end
+end  
 
 class Player
+ attr_accessor :name 
+
+ def initialize(name)
+ @name = name 
+ end
 
 end
 
+# Game Startup sequence
+
+puts "Welcome to Tygh's Hangman game!"
+sleep 0.5
+puts "Player, enter your first name, then return"
+name = gets.chomp
+sleep 0.5
+player = Player.new(name)
+puts "Good luck, #{name}"
+new_game = Hangman.new(player)
+new_game.display_board
