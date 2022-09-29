4cb4edbf1ec29deaa05cf8f543f1fec6adea31f4
Tygh Walters <tyghwalters@gmail.com>
2022-09-28 19:59:35 -0700

    Increment @round and stop game after 6

--- hangman.rb
+++ hangman.rb
@@ -1,16 +1,28 @@
 class Hangman 
   # load Google dictionary
   # randomly select a word between 5 and 12 characters long for the secret word.
-  attr_accessor :player, :secret_word
+  attr_accessor :player
 
-  def initialize(player, secret_word)
+  def initialize(player)
     @player = player
-    @secret_word = secret_word
-    puts "The Secret Word is #{secret_word}"
-    display_board
+    @round = 1 # play
+    
+    # secret word precessing
+    # read words txt file to array so we can pick random one
+    @words_list = File.read('google-10000-english-no-swears.txt').split 
+    # between 5 and 12 characters long
+    @filtered_words_list = @words_list.select { |word| word.length >= 5 && word.length <= 12 }
+    @secret_word = @filtered_words_list.sample
+    # create X num dashes to match chars on secret_word
+    @secret_word_array = @secret_word.split("")
+    puts "The Secret Word is #{@secret_word}"
+    # p secret_word_array
+    # display_array =  
+    display_board()
   end
 
-  def display_board
+  def display_board()
+    puts "Round: #{@round}"
     puts "Board: _ _ _ _ _ "
     play
   end
@@ -21,7 +33,13 @@ class Hangman
   end
 
   def winner_check
-    puts "Winner!"
+    if @round == 7
+      puts "You're hung, #{player.name} loses!"
+    else
+      #keep playing
+      @round = @round + 1
+      display_board()
+    end   
   end
 end  
 
@@ -35,7 +53,8 @@ class Player
   def guess
     puts "#{self.name}, what's your guess? (A-Z)"
       player_guess = gets.chomp
-      until /\p{L}/.match(player_guess) # is a letter (case insensitive)
+      # Ensure guess is a letter (case insensitive) and not more than 1
+      until /\p{L}/.match(player_guess) && player_guess.length == 1 
         puts "Oops, please type one letter, A-Z"
         player_guess = gets.chomp
       end
@@ -51,9 +70,4 @@ name = gets.chomp
 sleep 0.5
 player = Player.new(name)
 puts "Good luck, #{name}"
-# read words txt file to array so we can pick random one
-words_list = File.read('google-10000-english-no-swears.txt').split
-# between 5 and 12 characters long
-filtered_words_list = words_list.select { |word| word.length >= 5 && word.length <= 12 }
-secret_word = filtered_words_list.sample
-new_game = Hangman.new(player, secret_word)
+new_game = Hangman.new(player)
