d6faebcd0a11f42adce368d4bc66a12537516ca2
Tygh Walters <tyghwalters@gmail.com>
2022-09-28 17:28:24 -0700

    Download txt file and set Secret Word

--- hangman.rb
+++ hangman.rb
@@ -4,8 +4,10 @@ class Hangman
   attr_accessor :player 
 
   def initialize(player)
-  @player = player 
+    @player = player 
   end
+
+  
 
   def display_board
     puts "Board: _ _ _ _ _ "
@@ -18,16 +20,15 @@ class Hangman
 end  
 
 class Player
- attr_accessor :name 
+  attr_accessor :name
 
- def initialize(name)
- @name = name 
- end
+  def initialize(name)
+    @name = name 
+  end
 
 end
 
 # Game Startup sequence
-
 puts "Welcome to Tygh's Hangman game!"
 sleep 0.5
 puts "Player, enter your first name, then return"
@@ -36,4 +37,8 @@ sleep 0.5
 player = Player.new(name)
 puts "Good luck, #{name}"
 new_game = Hangman.new(player)
+# read words txt file to array so we can pick random one
+words_list = File.read('google-10000-english-no-swears.txt').split
+secret_word = words_list.sample
+puts "The Secret Word is #{secret_word}"
 new_game.display_board
