ab8c69c8c82837f4b2da7a751b0a7d851df6c860
Tygh Walters <tyghwalters@gmail.com>
2022-09-28 18:12:59 -0700

    Fix player guess inupt
    Add regex to check that the player's guess is a case insensitive letter

--- hangman.rb
+++ hangman.rb
@@ -6,12 +6,17 @@ class Hangman
   def initialize(player, secret_word)
     @player = player
     @secret_word = secret_word
+    puts "The Secret Word is #{secret_word}"
     display_board
   end
 
   def display_board
-    puts "The Secret Word is #{secret_word}"
     puts "Board: _ _ _ _ _ "
+    play
+  end
+
+  def play(letter = player.guess)
+    # puts "the letter class is #{letter.class}"
     winner_check
   end
 
@@ -24,7 +29,16 @@ class Player
   attr_accessor :name
 
   def initialize(name)
-    @name = name 
+    @name = name
+  end
+
+  def guess
+    puts "#{self.name}, what's your guess? (A-Z)"
+      player_guess = gets.chomp
+      until /\p{L}/.match(player_guess) # is a letter (case insensitive)
+        puts "Oops, please type one letter, A-Z"
+        player_guess = gets.chomp
+      end
   end
 
 end
